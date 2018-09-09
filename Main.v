781cea6a6a8cb737f8efa9a84694ee3a95bb08e4
Maciej Bielecki <zyla@prati.pl>
2018-09-09 13:31:03 +0200

    Use Inductive for html extraction

--- Main.v
+++ Main.v
@@ -11,13 +11,17 @@ Require Import ExtrOcamlString.
 
 Module Html.
   Definition tag_name := string.
-  Parameter html : Set -> Set.
-  Parameter el_attr : forall {A}, string -> list (string * string) -> list (html A) -> html A.
-  Parameter text : forall {A}, string -> html A.
+
+  Inductive html (A : Set) : Type := (* TODO: Why does it have to be Type? *)
+    | Elem : string -> list (string * string) -> list (html A) -> html A
+    | Text : string -> html A.
+
+  Definition el_attr : forall {A}, string -> list (string * string) -> list (html A) -> html A := Elem.
+  Definition text : forall {A}, string -> html A := Text.
   Definition el {A} : tag_name -> list (html A) -> html A :=
     fun tag => el_attr tag [].
   Definition el_ {A} : tag_name -> html A :=
-    fun tag => el tag nil.
+    fun tag => el_attr tag [] [].
 End Html.
 
 (* App *)
@@ -43,14 +47,17 @@ Definition update (m : model) (msg : event) : model :=
 
 End App.
 
-Extract Constant Html.html "'a" => "'a Vdom.t".
-Extract Constant Html.el_attr =>
-  "(fun tag attrs -> Vdom.node (Utils.camlstring_of_coqstring tag)
+Extract Inductive Html.html => "Vdom.t" [
+  (* Elem *)
+  "(fun (tag, attrs, children) -> Vdom.node
+     (Utils.camlstring_of_coqstring tag)
      (List.map
        (function (k, v) -> Vdom.Attribute ("""", Utils.camlstring_of_coqstring k, Utils.camlstring_of_coqstring v))
-       attrs))".
-Extract Constant Html.text => "(fun t -> Vdom.text (Utils.camlstring_of_coqstring t))".
-
+       attrs)
+     children)"
+  (* Text *)
+  "(fun t -> Vdom.text (Utils.camlstring_of_coqstring t))"
+  ].
 
 Extraction "src/app.ml" App.
 
