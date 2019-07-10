eb980a862b5e7bee186b6c7c1c546da0c862641d
Maciej Bielecki <zyla@prati.pl>
2019-07-10 16:36:48 +0200

    Add event handling

--- Main.v
+++ Main.v
@@ -17,17 +17,28 @@ Local Open Scope record.
 Module Html.
   Definition tag_name := string.
 
+  Inductive prop (A : Set) : Set :=
+    | Attr : string -> string -> prop A
+    | Event : string -> A -> prop A.
+
+  Arguments Attr {_}.
+
   Inductive html (A : Set) : Set :=
-    | Elem : string -> list (string * string) -> list (html A) -> html A
+    | Elem : string -> list (prop A) -> list (html A) -> html A
     | Text : string -> html A.
 
-  Definition el_attr : forall {A}, string -> list (string * string) -> list (html A) -> html A
+  Definition el_attr : forall {A}, string -> list (prop A) -> list (html A) -> html A
     := Elem.
   Definition text : forall {A}, string -> html A := Text.
   Definition el {A} : tag_name -> list (html A) -> html A :=
     fun tag => el_attr tag [].
   Definition el_ {A} : tag_name -> html A :=
     fun tag => el_attr tag [] [].
+
+  Notation "k =: v" := (Attr k v) (at level 75).
+
+  Definition on : forall {A : Set}, string -> A -> prop A := Event.
+
 End Html.
 
 (* App *)
@@ -45,7 +56,8 @@ Definition model :=
    @}.
    
 Inductive event :=
-  | SetFirstName : string -> event.
+  | TweakName : event
+  | Noop : event.
 
 Definition init (_ : unit) : model :=
   {# _first_name :- "John"
@@ -54,26 +66,36 @@ Definition init (_ : unit) : model :=
 
 Definition view (m : model) : html event :=
   el "div"
-    [ el_attr "h2" [ ("style", "color: red;") ] [ text "Hello world!!!!!" ]
-    ; el_attr "p" [ ("style", "font-size: 300%;") ] [ text ("Hello, " ++ (m !! _first_name) ++ "!") ]
+    [ el_attr "h2" [ "style"=:"color: red;" ] [ text "Hello world!!!!!" ]
+    ; el_attr "p" [ "style"=:"font-size: 300%;" ] [ text ("Hello, " ++ (m !! _first_name) ++ "!") ]
     ; el "p" [ text ("Hello again, " ++ (m !! _last_name)) ]
     ; el "strong" [ text "lol" ]
+    ; el "p"
+      [ el_attr "button" [ on "click" TweakName ] [ text "Change" ] ]
     ].
 
 Definition update (m : model) (msg : event) : model :=
   match msg with
-  | SetFirstName x => {# m with _first_name :- x #}
+  | TweakName =>
+      let new_name := if string_dec (m !! _first_name) "John" then "Alice" else "John"
+      in {# m with _first_name :- new_name #}
+  | Noop => m
   end.
 
 End App.
+
+Extract Inductive Html.prop => "Vdom.property" [
+  (* attr *)
+  "(function (k, v) -> Vdom.Attribute ("""", Utils.camlstring_of_coqstring k, Utils.camlstring_of_coqstring v))"
+  (* Event *)
+  "(function (name, msg) -> Vdom.onMsg (Utils.camlstring_of_coqstring name) msg)"
+].
 
 Extract Inductive Html.html => "Vdom.t" [
   (* Elem *)
   "(fun (tag, attrs, children) -> Vdom.node
      (Utils.camlstring_of_coqstring tag)
-     (List.map
-       (function (k, v) -> Vdom.Attribute ("""", Utils.camlstring_of_coqstring k, Utils.camlstring_of_coqstring v))
-       attrs)
+     attrs
      children)"
   (* Text *)
   "(fun t -> Vdom.text (Utils.camlstring_of_coqstring t))"
